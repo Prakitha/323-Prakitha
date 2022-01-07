fdf7ce3f9fdaf373015712f276198434cb2363f0
Prakitha <97210182+Prakitha@users.noreply.github.com>
2022-01-07 19:13:19 +0530

    Add files via upload

---
+++ Method1.java
@@ -0,0 +1,10 @@
+public class Method1 {
+    public  static void main(String[] args){
+        Customera customername=new Customera("Saamrudh","white");
+        customername.name="samrudh";
+        customername.colour="white";
+        System.out.println(customername.name+customername.colour);
+        
+      
+    }
+}
---
+++ maximum.java
@@ -0,0 +1,8 @@
+class maximum{
+public static void main(String[] args){
+int a=25;
+int b=45;
+int c=Math.max(a,b);
+System.out.println("maximum value="+c);
+}
+}
