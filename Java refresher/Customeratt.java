0267b7788802a969f318048b6509d2ed180abc61
Prakitha <97210182+Prakitha@users.noreply.github.com>
2022-01-11 18:29:00 +0530

    commit changes

---
+++ Java refresher/Customeratt.java
@@ -0,0 +1,19 @@
+public class Customeratt {
+    String name="Prakitha";
+    int age=20;
+    int dob=38;
+    String colour= "fair";
+    int height=6;
+    
+    
+    public Customeratt(String name,String colour) {
+        name="'prakitha";
+        colour="fair";
+        System.out.println(name +  colour);
+        System.out.println(dob);
+    }
+
+}
+
+
+
