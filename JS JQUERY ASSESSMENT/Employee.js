2fa5554d73259401b328c35dced43408221a36c1
Prakitha <97210182+Prakitha@users.noreply.github.com>
2022-01-11 18:41:18 +0530

    commit changes

---
+++ JS JQUERY ASSESSMENT/Employee.js
@@ -0,0 +1,14 @@
+class Emplo{
+    constructor(employeeId,employeeName,employeeAge,employeeSalary,employeeGender){
+        this.employeeId=employeeId;
+        this.employeeName=employeeName;
+        this.employeeAge=employeeAge;
+        this.employeeSalary=employeeSalary;
+        this.employeeGender=employeeGender;
+    }
+}
+
+var emp1 = new Emplo(1,"Prakitha",21,2500000,"F");
+var emp2 = new Emplo(2,"Samrudh",26,5000000,"M");
+var emp3 = new Emplo(3,"Manoritha",23,2800000,"F");
+var emp4 = new Emplo(4,"Mahesh RAj",23,5600000,"M");
